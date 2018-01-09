e7964215a2ed82ce2a81a51acfec37d413c271ec
Luc Hondareyte <luc.hondareyte@laposte.net>
2018-01-09 17:56:14 +0100

    Adding firmware: basic on/off

---
+++ firmware/power-button.c
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c)2017, Luc Hondareyte
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*
+ * POWER BUTTON - AVR Version
+ *
+ * Copyright (c)2017, Luc Hondareyte
+ * All rights reserved.
+ *
+ * $Id$
+ */
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include <util/delay.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include "power-button.h"
+
+// Time to wait operating systeme
+#define KERNEL_LOAD_TIMEOUT 20
+
+inline void SetupHardware(void ) {
+	//
+	// Set ports direction
+	D_PORT=0x00;
+	setBit(D_PORT,LED);
+	setBit(D_PORT,PWR);
+	// Pullup resistor on button pin
+	setBit(PORT,BUTTON);
+
+	// Active INT0
+#if defined (__AVR_ATmega328P__)
+	clearBit(EICRA,ISC00);
+	clearBit(EICRA,ISC01);
+	setBit(EIMSK,INT0);
+#else
+
+#endif
+	sei();
+}
+
+uint8_t board_is_on=0;
+uint8_t os_is_active=0;
+
+uint8_t volatile seconds=0;
+uint16_t volatile _ms=0x0000;
+
+#if defined (__AVR_ATtiny13a__)
+ISR (TIM0_OVF_vect) {
+#else
+ISR (TIMER0_OVF_vect) {
+#endif
+	_ms++;
+	if ( _ms == 1000 ) {
+		_ms=0;
+		seconds++;
+	}
+}
+
+ISR (INT0_vect) {
+	// Just Wake-up CPU
+}
+
+int startup(void) {
+	return 0;
+}
+
+inline void heartbeat(void) {
+}
+
+inline void startTimer(void) {
+}
+
+inline void stopTimer(void) {
+}
+
+uint8_t powerOn(void){
+	uint8_t rc=0;
+	//
+	// Wait for key pressed
+	//
+	loop_until_bit_is_clear(PORT,BUTTON);
+	switchOn();
+	_delay_ms(200);
+	startTimer();
+	//
+	// Allow immediate power off within 3 seconds after power on
+	//
+	while ( seconds <= 3 ) {
+		if ( bit_is_set (PORT,BUTTON) ) {
+			switchOff();
+			_delay_ms(200);
+			rc=1;
+			break;
+		}
+	}
+	stopTimer();
+	return(rc);
+}
+
+int main(void){
+	//
+	//
+	SetupHardware();
+	//
+	// Power-down configuration
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	switchOff();
+	board_is_on=NO;
+	os_is_active=NO;
+	while (1) {
+		sleep_enable();
+		sleep_mode();
+		sleep_disable();
+		/* 
+		   Waiting for INT0
+		   zzzzzzzzzzzzz.....
+		 */
+
+		if (board_is_on) {
+			switchOff();
+			board_is_on=NO;
+		}
+		else {
+			switchOn();
+			board_is_on=YES;
+		}
+		_delay_ms(100);
+		sleep_enable();
+		sleep_mode();
+		sleep_disable();
+	}
+}
+
