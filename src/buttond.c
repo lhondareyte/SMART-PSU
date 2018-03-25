98090c358afccf8e8b4bcce4da5116e3375a6ab2
Luc Hondareyte <luc.hondareyte@laposte.net>
2018-03-25 14:06:40 +0200

    Compile OK with gmake. Configuration file's parser works.

--- src/buttond.c
+++ src/buttond.c
@@ -4,36 +4,35 @@
 #include <libgpio.h>
 #include <time.h>
 
+#include "buttond.h"
+
 int main(int argc, char **argv)
 {
 	struct buttond_config buttond;
-	if ( (buttond = get_config(FILENAME) == -1 ) {
+	if (( get_config(CONFILE, &buttond)) == -1 ) {
 		return 1;
 	}
 
-#if defined(__DEBUG__)
-	fprintf(stderr, "Button Pin: %s\nButton command: %s\n", buttond.pin, buttond.cmd);
-#endif
-
-	gpio_handle_t handle;
-	int button_pin = buttond.pin;
+	int button_pin = atoi(buttond.pin);
 	int buttonstate=0;
 	int lastbuttonstate=0;
-#if defined(__DEBUG__)
-	int led_pin = 0;
-	int ledstate = GPIO_PIN_LOW;
-#endif
 
+	gpio_handle_t handle;
 	handle = gpio_open(0);
-
 	gpio_pin_input(handle, button_pin);
 	gpio_pin_low(handle, button_pin);
 	gpio_pin_pullup(handle, button_pin);
 
 #if defined(__DEBUG__)
+	fprintf(stderr, "Button Pin: %s\nButton command: %s\n", buttond.pin, buttond.cmd);
+	int led_pin = 0;
+	int ledstate = GPIO_PIN_LOW;
 	gpio_pin_output(handle, led_pin);
 	gpio_pin_low(handle, led_pin);
-#endif
+#endif	
+	lastbuttonstate = gpio_pin_get(handle, button_pin);
+
+	// Polling for key pressed
 	while(1) {
 		buttonstate = gpio_pin_get(handle, button_pin);
 		if (lastbuttonstate != buttonstate) {
@@ -41,7 +40,7 @@ int main(int argc, char **argv)
 			usleep(150000);
 
 #if defined(__DEBUG__)
-			//fprintf(stderr, "execl: %s\n", buttond.cmd);
+			fprintf(stderr, "execl: %s\n", buttond.cmd);
 #endif
 			execl ( "/sbin/shutdown", "shutdown", "-p", "now", NULL);
 		}
